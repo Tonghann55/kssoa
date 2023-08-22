8ea43ab104f6d14b379ab86d7dc6e86982726cae
Tonghann55 <124667799+Tonghann55@users.noreply.github.com>
2023-08-22 16:15:18 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`http://imeallrad.com/` + encodeURI(path as string)
+					`http://imeallrad.com/`
 				}`,
 			},
 		};
